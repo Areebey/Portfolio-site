4d42b02b901078500855af4c1e14ef76a480c112
areebey <areebrafiq125@gmail.com>
2025-02-12 14:27:36 +0500

    added projects links

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
-import project1 from "../assets/projects/project-1.jpg";
+import project1 from "../assets/projects/project-1.jpeg";
+import project02 from "../assets/projects/chat-app.png"
+import project002 from "../assets/projects/weather-app.jpeg"
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
@@ -59,60 +61,83 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Blog App",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    "Built a full-featured Blog App using React Router, Redux Toolkit, and Appwrite for backend services. Includes Signup/Signin, protected pages, a visual editor, image uploads, and more. Designed for scalability with a seamless user experience.",
+    technologies: ["HTML", "CSS", "Tailwindcss", "React","Appwrite"],
   },
   {
-    title: "Food Wagon Website",
-    image: project2,
+    title: "Chat App",
+    image: project02,
+    source: "https://fluffy-quokka-011a14.netlify.app/",
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "SaltnPepper Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Home and Guest",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
-  {
-    title: "Pepsi Website",
-    image: project5,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
-  {
-    title: "Tere-Web Website",
-    image: project6,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
-  {
-    title: "Basic Portfolio",
-    image: project7,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Built a React.js chat app with Firebase backend for real-time messaging. Features instantupdates, user authentication with google, and a responsive interface",
+    technologies: ["HTML", "CSS", "React", "Firebase"],
   },
   {
     title: "Doctor's Website",
     image: project8,
+    source: "https://my-doctorapp.netlify.app/",
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    "",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Weather App",
+    image: project002,
+    source: "https://weather-update-by-areeb.netlify.app/",
+    description:
+    "",
+    technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+  },
+  {
+    title: "Food Wagon Website",
+    image: project2,
+    source: "https://me-foodwagon-app.netlify.app/",
+    description:
+    "",
+    technologies: ["HTML", "CSS", "Bootstrap"],
+  },
+  {
+    title: "Home and Guest",
+    image: project4,
+    sourec: "https://legendary-daifuku-2bdc2f.netlify.app/",
+    description:
+    "",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Pepsi Website",
+    image: project5,
+    source: "https://mepepsi.netlify.app/",
+    description:
+    "",
+    technologies: ["HTML", "CSS"],
+  },
+  {
+    title: "Tere-Web Website",
+    image: project6,
+    source: "https://my-ride-share.netlify.app/",
+    description:
+    "",
+    technologies: ["HTML", "CSS","Bootstrap"],
+  },
+  {
+    title: "SaltnPepper Website",
+    image: project3,
+    source: "https://mesaltnpepper.netlify.app/",
+    description:
+      "A basic website",
+    technologies: ["HTML", "CSS"],
+  },
+  {
+    title: "Basic Portfolio",
+    image: project7,
+    source: "https://meek-crepe-bf9a13.netlify.app/",
+    description:
+    "A personal portfolio website showcasing projects, skills, and contact information",
+    technologies: ["HTML", "CSS"],
   },
 ];
 
